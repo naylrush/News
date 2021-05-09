bcc23d100d1f91c0f931f9b30ce51d8bf13f55b7
Alexey Davletshin <naylrush@gmail.com>
2021-05-09 13:01:09 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Alexey Davletshin on 09.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
